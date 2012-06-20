f21a5ecb699fefc348dd29fcaf3955f336c1e771
Omar Estrella <omar@caktusgroup.com>
2012-06-20 12:48:20 -0400

    Adjust to use inlusion_tag

--- carousel/templatetags/carousel_tags.py
+++ carousel/templatetags/carousel_tags.py
@@ -5,30 +5,13 @@ from carousel.models import Carousel
 register = template.Library()
 
 
-@register.tag('carousel')
-def show_carousel(parser, token):
+@register.inclusion_tag('carousel/carousel.html', takes_context=True)
+def show_carousel(context, slug, *args, **kwargs):
     try:
-        tag_name, function, carousel_slug = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError('%r tag requires the carousel slug as an argument'
-            % token.contents.split()[0])
-    else:
-        if not (carousel_slug[0] == carousel_slug[-1] and carousel_slug[0] in ('"', "'")):
-            raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
-        return CarouselNode(carousel_slug)
-
-
-class CarouselNode(template.Node):
-    def __init__(self, carousel_slug):
-        self.carousel_slug = carousel_slug.strip('"')
-
-    def render(self, context):
-        if self.carousel_slug is not None:
-            try:
-                carousel = Carousel.objects.get(slug=self.carousel_slug)
-                context['carousel'] = carousel
-                return template.loader.render_to_string('carousel/carousel.html', context)
-            except Carousel.DoesNotExist:
-                return ''
-
+        carousel = Carousel.objects.get(slug=slug)
+        context['carousel'] = carousel
+    except Carousel.DoesNotExist:
         return ''
+    else:
+        pass
+    return context
